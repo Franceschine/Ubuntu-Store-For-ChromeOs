bf7cf611253a1eba089a77e24a5a9452bc50bfc9
Franceschine <149089432+Franceschine@users.noreply.github.com>
2024-07-23 15:29:03 +0200

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,125 +1,68 @@
 #!/bin/bash
-echo "Ubuntu store installer for crostini (13 functions)"
-echo "1 download files"
-sudo wget https://raw.githubusercontent.com/Franceschine/Ubuntu-Store-For-ChromeOs/main/cros.list
-echo "2 starting and upgrading package manager"
-sudo apt update
-sudo NEEDRESTART_MODE=a apt upgrade -y
-echo "3 add apt sources"
-sudo cp cros.list /etc/apt/sources.list.d/
-echo "4 add keys"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 78BD65473CB3BD13
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4EB27DB2A3B88B8B
-echo "5 download packages from google server"
-sudo apt update
-sudo apt download cros-ui-config cros-guest-tools cros-im cros-adapta cros-apt-config cros-garcon cros-host-fonts cros-notificationd cros-sommelier cros-logging cros-pipe-config cros-sommelier-config cros-sudo-config cros-systemd-overrides cros-vmstat-metrics cros-wayland cros-tast-tests cros-sftp cros-pulse-config cros-xdg-desktop-portal
-echo "6 download ubuntu store"
-sudo apt download gnome-software
-echo "7 install deps from ubuntu server"
-sudo NEEDRESTART_MODE=a apt install libgtk-3-0 -y
-sudo NEEDRESTART_MODE=a dpkg --configure -a
-echo "8 making apps and libs compatibile with ubuntu"
-sudo dpkg-deb -x  cros-im_*_amd64.deb 1
-sudo dpkg-deb --control  cros-im_*_amd64.deb 1/DEBIAN
-sudo sed -i 's/, qtbase-abi-5-15-2//g' 1/DEBIAN/control
-sudo dpkg -b 1 1.deb
-echo "9 install libs"
-echo "1/7"
-sudo NEEDRESTART_MODE=a apt install libd3dadapter9-mesa -y
-echo "2/7"
-sudo NEEDRESTART_MODE=a apt install libxatracker2 -y
-echo "3/7"
-sudo NEEDRESTART_MODE=a apt install mesa-va-drivers -y
-echo "4/7"
-sudo NEEDRESTART_MODE=a apt install mesa-vdpau-drivers -y
-echo "5/7"
-sudo NEEDRESTART_MODE=a apt install mesa-vulkan-drivers -y
-echo "6/7 container support"
-sudo NEEDRESTART_MODE=a apt install libgl1-mesa-dri -y
-echo "7/7 cursor support"
-sudo NEEDRESTART_MODE=a apt install adwaita-icon-theme-full
-echo "10 install apps"
-echo "1/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-adapta_*_all.deb -y
-echo "2/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-ui-config_*_all.deb -y
-sudo dpkg --configure -a 
-sudo dpkg --force-overwrite -i cros-ui-config_*_all.deb 
-echo "3/20"
-sudo NEEDRESTART_MODE=a apt install ./1.deb -y
-echo "4/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-apt-config_*_all.deb -y
-echo "4/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-garcon_*_all.deb -y
-echo "6/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-host-fonts_*_all.deb -y
-echo "7/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-notificationd_*_all.deb -y
-echo "8/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-sommelier_*_all.deb -y
-echo "9/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-logging_*_all.deb -y
-echo "10/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-pipe-config_*_all.deb -y
-echo "11/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-sommelier-config_*_all.deb -y
-echo "12/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-sudo-config_*_all.deb -y
-echo "13/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-systemd-overrides_*_all.deb -y
-echo "14/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-vmstat-metrics_*_all.deb -y
-echo "15/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-wayland_*_all.deb -y
-echo "16/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-pulse-config_*_all.deb -y
-echo "17/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-guest-tools_*_all.deb -y
-echo "18/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-tast-tests_*_all.deb -y
-echo "19/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-sftp_*_all.deb -y
-echo "20/20"
-sudo NEEDRESTART_MODE=a apt install ./cros-xdg-desktop-portal_*_all.deb -y
-echo "11 install gui store"
-sudo NEEDRESTART_MODE=a apt install ./gnome-software_*_amd64.deb -y
-echo "12 copy files and user"
-echo "1/18"
-sudo mkdir -p /opt/google/cros-containers/bin/sommelier
-echo "2/18"
-sudo mkdir -p /opt/google/cros-containers/lib/
-echo "3/18"
-sudo cp /usr/lib/x86_64-linux-gnu/dri/swrast_dri.so /opt/google/cros-containers/lib/
-echo "4/18"
-sudo sed -i '1cpenguin' /etc/hostname
-echo "5/18"
-killall -u ubuntu
-echo "6/18"
-groupmod -n linux ubuntu
-echo "7/18"
-usermod -md /home/linux -l linux ubuntu
-echo "8/18"
-usermod -aG users linux
-echo "9/18"
-loginctl enable-linger linux
-echo "10/18"
-sed -i 's/ubuntu/linux/' /etc/sudoers.d/90-cloud-init-users
-echo "11/18"
-rmdir ~/Downloads
-echo "12/18"
-ln -sf /mnt/chromeos/MyFiles ~
-echo "13/18"
-sudo update-alternatives --set x-www-browser /usr/bin/garcon-url-handler
-echo "14/18"
-sudo update-alternatives --set www-browser /usr/bin/garcon-url-handler
-echo "15/18"
-sudo xdg-mime default garcon_host_browser.desktop x-scheme-handler/http
-echo "16/18"
-sudo xdg-mime default garcon_host_browser.desktop x-scheme-handler/https
-echo "17/18"
-sudo xdg-mime default garcon_host_browser.desktop text/html
-echo "18/18"
-sudo cp update-cros /usr/local/sbin
-echo "13 cleanup and reboot container"
-sudo rm -rf /root/*
-sudo reboot
+echo "Ubuntu store installer for crostini (60 commands)"
+echo "1" && sudo wget https://raw.githubusercontent.com/Franceschine/Ubuntu-Store-For-ChromeOs/main/cros.list
+echo "2" && sudo wget https://raw.githubusercontent.com/Franceschine/Ubuntu-Store-For-ChromeOs/main/update-cros
+echo "3" && sudo apt update
+echo "4" && sudo NEEDRESTART_MODE=a apt upgrade -y
+echo "5" && sudo cp cros.list /etc/apt/sources.list.d/
+echo "6" && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 78BD65473CB3BD13
+echo "7" && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4EB27DB2A3B88B8B
+echo "8" && sudo apt update
+echo "9" && sudo apt download cros-ui-config cros-guest-tools cros-im cros-adapta cros-apt-config cros-garcon cros-host-fonts cros-notificationd cros-sommelier cros-logging cros-pipe-config cros-sommelier-config cros-sudo-config cros-systemd-overrides cros-vmstat-metrics cros-wayland cros-tast-tests cros-sftp cros-pulse-config cros-xdg-desktop-portal
+echo "10" && sudo apt download gnome-software
+echo "11" && sudo NEEDRESTART_MODE=a apt install libgtk-3-0 -y
+echo "12" && sudo NEEDRESTART_MODE=a dpkg --configure -a
+echo "13" && sudo dpkg-deb -x  cros-im_*_amd64.deb 1
+echo "14" && sudo dpkg-deb --control  cros-im_*_amd64.deb 1/DEBIAN
+echo "15" && sudo sed -i 's/, qtbase-abi-5-15-2//g' 1/DEBIAN/control
+echo "16" && sudo dpkg -b 1 1.deb
+echo "17" && sudo NEEDRESTART_MODE=a apt install libd3dadapter9-mesa -y
+echo "18" && sudo NEEDRESTART_MODE=a apt install libxatracker2 -y
+echo "19" && sudo NEEDRESTART_MODE=a apt install mesa-va-drivers -y
+echo "20" && sudo NEEDRESTART_MODE=a apt install mesa-vdpau-drivers -y
+echo "21" && sudo NEEDRESTART_MODE=a apt install mesa-vulkan-drivers -y
+echo "22" && sudo NEEDRESTART_MODE=a apt install libgl1-mesa-dri -y
+echo "23" && sudo NEEDRESTART_MODE=a apt install adwaita-icon-theme-full
+echo "24" && sudo NEEDRESTART_MODE=a apt install ./cros-adapta_*_all.deb -y
+echo "25" && sudo NEEDRESTART_MODE=a apt install ./cros-ui-config_*_all.deb -y
+echo "26" && sudo dpkg --configure -a 
+echo "27" && sudo dpkg --force-overwrite -i cros-ui-config_*_all.deb 
+echo "28" && sudo NEEDRESTART_MODE=a apt install ./1.deb -y
+echo "29" && sudo NEEDRESTART_MODE=a apt install ./cros-apt-config_*_all.deb -y
+echo "30" && sudo NEEDRESTART_MODE=a apt install ./cros-garcon_*_all.deb -y
+echo "31" && sudo NEEDRESTART_MODE=a apt install ./cros-host-fonts_*_all.deb -y
+echo "32" && sudo NEEDRESTART_MODE=a apt install ./cros-notificationd_*_all.deb -y
+echo "33" && sudo NEEDRESTART_MODE=a apt install ./cros-sommelier_*_all.deb -y
+echo "34" && sudo NEEDRESTART_MODE=a apt install ./cros-logging_*_all.deb -y
+echo "35" && sudo NEEDRESTART_MODE=a apt install ./cros-pipe-config_*_all.deb -y
+echo "36" && sudo NEEDRESTART_MODE=a apt install ./cros-sommelier-config_*_all.deb -y
+echo "37" && sudo NEEDRESTART_MODE=a apt install ./cros-sudo-config_*_all.deb -y
+echo "38" && sudo NEEDRESTART_MODE=a apt install ./cros-systemd-overrides_*_all.deb -y
+echo "39" && sudo NEEDRESTART_MODE=a apt install ./cros-vmstat-metrics_*_all.deb -y
+echo "40" && sudo NEEDRESTART_MODE=a apt install ./cros-wayland_*_all.deb -y
+echo "41" && sudo NEEDRESTART_MODE=a apt install ./cros-pulse-config_*_all.deb -y
+echo "42" && sudo NEEDRESTART_MODE=a apt install ./cros-guest-tools_*_all.deb -y
+echo "43" && sudo NEEDRESTART_MODE=a apt install ./cros-tast-tests_*_all.deb -y
+echo "44" && sudo NEEDRESTART_MODE=a apt install ./cros-sftp_*_all.deb -y
+echo "45" && sudo NEEDRESTART_MODE=a apt install ./cros-xdg-desktop-portal_*_all.deb -y
+echo "46" && sudo NEEDRESTART_MODE=a apt install ./gnome-software_*_amd64.deb -y
+echo "47" && sudo mkdir -p /opt/google/cros-containers/bin/sommelier
+echo "48" && sudo mkdir -p /opt/google/cros-containers/lib/
+echo "49" && sudo cp /usr/lib/x86_64-linux-gnu/dri/swrast_dri.so /opt/google/cros-containers/lib/
+echo "50" && sudo sed -i '1cpenguin' /etc/hostname
+echo "51" && killall -u ubuntu
+echo "52" && groupmod -n linux ubuntu
+echo "53" && usermod -md /home/linux -l linux ubuntu
+echo "54" && usermod -aG users linux
+echo "55" && loginctl enable-linger linux
+echo "56" && sed -i 's/ubuntu/linux/' /etc/sudoers.d/90-cloud-init-users
+echo "57" && rmdir ~/Downloads
+echo "58" && ln -sf /mnt/chromeos/MyFiles ~
+echo "59" && sudo update-alternatives --set x-www-browser /usr/bin/garcon-url-handler
+echo "60" && sudo update-alternatives --set www-browser /usr/bin/garcon-url-handler
+echo "61" && sudo xdg-mime default garcon_host_browser.desktop x-scheme-handler/http
+echo "62" && sudo xdg-mime default garcon_host_browser.desktop x-scheme-handler/https
+echo "63" && sudo xdg-mime default garcon_host_browser.desktop text/html
+echo "64" && sudo cp update-cros /usr/local/sbin
+echo "65" && sudo rm -rf /root/*
+echo "66" && sudo reboot
